f336ed5577609eb50929172d5c08037b8db6897e
Sandeep Kumar <skrudra12@gmail.com>
2018-09-25 17:37:11 +0530

    Initial commit: Setup baseline with angular and spring boot

---
+++ src/test/java/com/sba6/srm/SrmApplicationTests.java
@@ -0,0 +1,16 @@
+package com.sba6.srm;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SrmApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
