0719989c7cdc3c9d76be594a7ff4b6dd7400ea6c
Sandeep Kumar <skrudra12@gmail.com>
2018-09-27 20:50:31 +0530

    Adding data.sql, entity naming convention changes

--- src/main/java/com/sba6/srm/entity/LoginDetail.java
+++ src/main/java/com/sba6/srm/entity/LoginDetail.java
@@ -1,18 +1,25 @@
 package com.sba6.srm.entity;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 import lombok.*;
 
 @Entity
-@Getter @Setter
-@NoArgsConstructor
-public class LoginDetail {
+@Table(name="login_detail")
+public @Data class LoginDetail {
 	@Id @GeneratedValue
-	private int empId;
+	@Column
+	private Long id;
+	@Column
+	private Long empId;
+	@Column
 	private String userName;
+	@Column
 	private String password;
-	private int role;
+	@Column
+	private Long role;
 }
