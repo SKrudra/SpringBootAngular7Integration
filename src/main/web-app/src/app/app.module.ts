1636854e006b254ced8137450fef9e9fa0240434
Satyanarayana paidi <satyanarayana.paidi@ggktech.local>
2018-09-28 12:55:04 +0530

    Created view components

--- src/main/web-app/src/app/app.module.ts
+++ src/main/web-app/src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { EmployeeDetailComponent } from './employee-detail/employee-detail.component';
+import { BaseViewComponent } from './base-view/base-view.component';
+import { RequestComponent } from './request/request.component';
+import { ManagerDashboardComponent } from './manager-dashboard/manager-dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmployeeDetailComponent,
+    BaseViewComponent,
+    RequestComponent,
+    ManagerDashboardComponent
   ],
   imports: [
     BrowserModule
---
+++ src/main/web-app/src/app/employee-detail/employee-detail.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-employee-detail',
+  templateUrl: './employee-detail.component.html',
+  styleUrls: ['./employee-detail.component.css']
+})
+export class EmployeeDetailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
