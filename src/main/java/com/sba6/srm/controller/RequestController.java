89c4b908d5a341bfc40a99258a9b44a39cb12b9a
sanket.nimje <sanket.nimje@GGKU3SYS193.ggktech.local>
2018-10-11 14:46:52 +0530

    format

--- src/main/java/com/sba6/srm/controller/RequestController.java
+++ src/main/java/com/sba6/srm/controller/RequestController.java
@@ -42,68 +42,64 @@ GET/api/empMgr/{empId}
  */
 @RestController
 public class RequestController {
-	
-	@Autowired
-	private EmployeeService employeeService;
-	@Autowired
-	private RequestService requestService;
-	@Autowired
-	private EmailService emailService;
-	
-	
-	//1. GET Requests for manager GET/api/requests/{mgrId} 
-	@Transactional
-	@RequestMapping(value = "/api/requests/{mgrId}", method = RequestMethod.GET)
-	public List<Request> getRequestsForManager(@PathVariable Long mgrId){
-		List<Request> requestResult =  requestService.getRequestsForManager(mgrId);
-		emailService.sendMail("emailid", "Request Created", "Someone asked for employee detausk");
-		return requestResult;
-	}
-	
-	//2. Update Request Status PUT/api/request
-	@RequestMapping(value="/api/request", method=RequestMethod.PUT,produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
-	public ResponseEntity updateRequestStatus(@RequestBody Request updateRequest){
-		Request req = requestService.getRequestById(updateRequest.getId());
-		req.setStatus(updateRequest.getStatus());
-		if(updateRequest.getComment()!=null && updateRequest.getComment()!="")
-		{
-			req.setComment(updateRequest.getComment());
-		}
-		requestService.updateRequest(req);
-		return new ResponseEntity(HttpStatus.OK);
-	}
-	
-	//3. Get Request for employee GET/api/emp/requests/{empId}
-	@RequestMapping(value="/api/emp/requests/{empId}", method=RequestMethod.GET)
-	public Request getRequest(@PathVariable Long empId){
-		emailService.sendMail("emailid@gmail.com", "Request Created", "Someone asked for employee detausk");
-		return requestService.getRequestForEmployee(empId);
-	}
-	
-	//4. create request for employee POST/api/request
-	@RequestMapping(value="/api/request", method=RequestMethod.POST)
-	public ResponseEntity createRequest(@RequestBody Request newRequest){
-		Employee emp = new Employee();
-		emp = employeeService.getEmployee(newRequest.getEmployee().getId());
-		Request req =newRequest;
-		req.setEmployee(emp);
-		requestService.addRequest(req);
-		emailService.sendMail("emailid@gmail.com", "Request Created", "Someone created a request");
-		return new ResponseEntity(HttpStatus.OK);
-	}
-	
-	//5. Get employee details GET/api/emp/{empId}
-	@RequestMapping(value="/api/emp/{empId}", method = RequestMethod.GET)
-	public Employee getEmployee(@PathVariable Long empId){
-		return employeeService.getEmployee(empId);
-	}
-	
- 
-	//6. Get employee's manager details GET/api/empMgr/{empId}
-   @RequestMapping(value="/api/empMgr/{empId}", method = RequestMethod.GET)
-   public Employee getEmployeeManager(@PathVariable Long empId){
+
+   @Autowired
+   private EmployeeService employeeService;
+   @Autowired
+   private RequestService requestService;
+   @Autowired
+   private EmailService emailService;
+
+   // 1. GET Requests for manager GET/api/requests/{mgrId}
+   @Transactional
+   @RequestMapping(value = "/api/requests/{mgrId}", method = RequestMethod.GET)
+   public List<Request> getRequestsForManager(@PathVariable Long mgrId) {
+      List<Request> requestResult = requestService.getRequestsForManager(mgrId);
+      emailService.sendMail("emailid", "Request Created", "Someone asked for employee detausk");
+      return requestResult;
+   }
+
+   // 2. Update Request Status PUT/api/request
+   @RequestMapping(value = "/api/request", method = RequestMethod.PUT, produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
+   public ResponseEntity updateRequestStatus(@RequestBody Request updateRequest) {
+      Request req = requestService.getRequestById(updateRequest.getId());
+      req.setStatus(updateRequest.getStatus());
+      if (updateRequest.getComment() != null && updateRequest.getComment() != "") {
+         req.setComment(updateRequest.getComment());
+      }
+      requestService.updateRequest(req);
+      return new ResponseEntity(HttpStatus.OK);
+   }
+
+   // 3. Get Request for employee GET/api/emp/requests/{empId}
+   @RequestMapping(value = "/api/emp/requests/{empId}", method = RequestMethod.GET)
+   public Request getRequest(@PathVariable Long empId) {
+      emailService.sendMail("emailid@gmail.com", "Request Created", "Someone asked for employee detausk");
+      return requestService.getRequestForEmployee(empId);
+   }
+
+   // 4. create request for employee POST/api/request
+   @RequestMapping(value = "/api/request", method = RequestMethod.POST)
+   public ResponseEntity createRequest(@RequestBody Request newRequest) {
+      Employee emp = new Employee();
+      emp = employeeService.getEmployee(newRequest.getEmployee().getId());
+      Request req = newRequest;
+      req.setEmployee(emp);
+      requestService.addRequest(req);
+      emailService.sendMail("emailid@gmail.com", "Request Created", "Someone created a request");
+      return new ResponseEntity(HttpStatus.OK);
+   }
+
+   // 5. Get employee details GET/api/emp/{empId}
+   @RequestMapping(value = "/api/emp/{empId}", method = RequestMethod.GET)
+   public Employee getEmployee(@PathVariable Long empId) {
+      return employeeService.getEmployee(empId);
+   }
+
+   // 6. Get employee's manager details GET/api/empMgr/{empId}
+   @RequestMapping(value = "/api/empMgr/{empId}", method = RequestMethod.GET)
+   public Employee getEmployeeManager(@PathVariable Long empId) {
       return employeeService.getEmployeeManager(empId);
    }
-	
-	
+
 }
