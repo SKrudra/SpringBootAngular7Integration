e032421739da0fd6f0969344b7dd651dc8d19889
Satyanarayana paidi <satyanarayana.paidi@ggktech.local>
2018-10-11 22:14:41 +0530

    Added spring security related classes.

--- src/main/java/com/sba6/srm/config/SrmSecurityConfig.java
+++ src/main/java/com/sba6/srm/config/SrmSecurityConfig.java
@@ -1,30 +1,58 @@
 package com.sba6.srm.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.User.UserBuilder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SrmSecurityConfig extends WebSecurityConfigurerAdapter {
 
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-		http.csrf().disable();
-	}
+		
+	@Autowired
+    private UserDetailsService userDetailsService;
 
-	@Override
-	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		UserBuilder users = User.withDefaultPasswordEncoder();
-		auth.inMemoryAuthentication()
-			.withUser(users.username("sp").password("sp").roles("MANAGER"))
-			.withUser(users.username("ms").password("ms").roles("EMPLOYEE"))
-			.withUser(users.username("bw").password("bw").roles("EMPLOYEE"))
-			.withUser(users.username("ck").password("ck").roles("EMPLOYEE"));
-	}
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+	        .csrf()
+	        .disable()
+	        .authorizeRequests()
+	            .antMatchers("/resources/**", "/users/","/","/users/newUser/**").permitAll()
+	            .anyRequest().authenticated()
+	            .and()
+	        .formLogin()
+	           // .loginPage("/login")
+	            .permitAll()
+	            .and()
+	        .logout()
+	            .permitAll();
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder());
+    }
+    
+    @Bean
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
 
 }
