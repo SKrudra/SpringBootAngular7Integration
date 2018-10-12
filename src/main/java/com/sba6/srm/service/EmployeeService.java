bc0dd64ed72405354801d2453183b3404ae60936
Sandeep Kumar <skrudra12@gmail.com>
2018-10-12 21:40:16 +0530

    code refactor

--- src/main/java/com/sba6/srm/service/EmployeeService.java
+++ src/main/java/com/sba6/srm/service/EmployeeService.java
@@ -11,19 +11,21 @@ import com.sba6.srm.repository.EmployeeRepository;
 @Service
 public class EmployeeService {
 
-   @Autowired
-   private EmployeeRepository employeeRepository;
+	@Autowired
+	private EmployeeRepository employeeRepository;
 
-   public Employee getEmployee(Long id) {
-      Employee employee = employeeRepository.findById(id).get();
-      Employee manager = null;
-      if (employee.getMgrId() != null) {
-         manager = employeeRepository.findById(employee.getMgrId()).get();
-         employee.setManagerName(manager.getFirstName() + " " + manager.getLastName());
-      } else {
-         employee.setManagerName("");
-      }
-      return employee;
-   }
+	public Employee getEmployee(Long id) {
+		Employee employee = null;
+		Optional<Employee> opt = employeeRepository.findById(id);
+		if (opt.isPresent()) {
+			employee = opt.get();
+			Employee manager;
+			if (employee.getMgrId() != null) {
+				manager = employeeRepository.findById(employee.getMgrId()).get();
+				employee.setManagerName(manager.getFirstName() + " " + manager.getLastName());
+			}
+		}
+		return employee;
+	}
 
 }
